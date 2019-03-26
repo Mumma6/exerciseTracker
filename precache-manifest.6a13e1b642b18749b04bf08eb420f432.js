a22865f503d04e831bbe9bb709474a0aac2af020
Martin Persson <moarte6@gmail.com>
2019-03-26 16:35:30 +0100

    Updates

---
+++ precache-manifest.6a13e1b642b18749b04bf08eb420f432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18c3dafd10df1282645c",
+    "url": "/Mumma6/exerciseTracker/static/js/runtime~main.1877c286.js"
+  },
+  {
+    "revision": "634e17d550688aad047f",
+    "url": "/Mumma6/exerciseTracker/static/js/main.09091e1a.chunk.js"
+  },
+  {
+    "revision": "61cdcd0a6aab69f7a09f",
+    "url": "/Mumma6/exerciseTracker/static/js/2.ce415601.chunk.js"
+  },
+  {
+    "revision": "634e17d550688aad047f",
+    "url": "/Mumma6/exerciseTracker/static/css/main.82f985ed.chunk.css"
+  },
+  {
+    "revision": "61cdcd0a6aab69f7a09f",
+    "url": "/Mumma6/exerciseTracker/static/css/2.65fabcac.chunk.css"
+  },
+  {
+    "revision": "fd0c7a9df16d12b1532ddb90f4a8cf60",
+    "url": "/Mumma6/exerciseTracker/index.html"
+  }
+];
